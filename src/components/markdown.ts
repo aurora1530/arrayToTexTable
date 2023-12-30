4e64ba34ea95170fc08a8fb9e5e469fc46c70bea
aurora1530 <k.0523.y.1530@gmail.com>
2023-12-30 14:55:00 +0900

    Refactor markdownToArray function to improve readability and simplify alignment logic

--- src/components/markdown.ts
+++ src/components/markdown.ts
@@ -10,19 +10,15 @@ export type MarkdownArray = {
  * @returns 変換された配列とセルの配置情報を含むオブジェクト
  */
 export function markdownToArray(mdTable: string): MarkdownArray {
-  const lines = mdTable.split('\n').map((line) => line.replace(/^\||\|$/g, ''));
-  const align: ('l' | 'c' | 'r')[] = lines[1].split('|').map((cell) => {
-    switch (cell.trim()) {
-      case ':---':
-        return 'l';
-      case ':---:':
-        return 'c';
-      case '---:':
-        return 'r';
-      default:
-        return 'c';
-    }
-  });
+  const lines = mdTable.split('\n').map((line) => line.replace(/^\||\|$/g, '')); //この後"|"でsplitするので、先頭と末尾の|を削除
+  const align: ('l' | 'c' | 'r')[] =
+    lines[1]?.split('|').map((cell) => {
+      cell = cell.trim();
+      if (cell.startsWith(':') && cell.endsWith(':')) return 'c';
+      if (cell.startsWith(':')) return 'l';
+      if (cell.endsWith(':')) return 'r';
+      return 'c';
+    }) ?? 'c'.repeat(lines[0].split('|').length).split('');
   const table = [
     lines[0].split('|').map((cell) => cell.trim()),
     ...lines.slice(2).map((row) => row.split('|').map((cell) => cell.trim())),
